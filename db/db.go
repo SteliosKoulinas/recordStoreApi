d060ca1ab05c0a4769b69725a5f50ec1ffdb5ec8
Stelios Koulinas <kos@entersoft.gr>
2025-12-22 11:26:57 +0200

    add changes from office desk

--- db/db.go
+++ db/db.go
@@ -4,16 +4,20 @@ import (
 	"fmt"
 	"log"
 
-	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	_ "modernc.org/sqlite"
 )
 
 var DB *gorm.DB
 
 func Connect() {
-	dsn := "host=localhost user=stylianoskoulinas password=zPMa]W?dxY5M dbname=musicdb port=5432 sslmode=disable"
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	/*dsn := "host=localhost user=stylianoskoulinas password=zPMa]W?dxY5M dbname=musicdb port=5432 sslmode=disable"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})*/
+
+	db, err := gorm.Open(sqlite.Open("records.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database: ", err)
 	}
